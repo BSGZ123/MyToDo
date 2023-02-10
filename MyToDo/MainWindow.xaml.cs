e83b4dbc95e19542b980962dd8e5e76f8e5fb072
BsKPLu <bianshuiguangzhong@gmail.com>
2023-02-10 20:48:07 +0800

    UI主界面操作逻辑

--- MyToDo/MainWindow.xaml.cs
+++ MyToDo/MainWindow.xaml.cs
@@ -23,6 +23,39 @@ namespace MyToDo
         public MainWindow()
         {
             InitializeComponent();
+
+            /*
+             * 界面按钮操作实现逻辑
+             */
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) => 
+            { if(this.WindowState == WindowState.Maximized)
+                    this.WindowState = WindowState.Normal;
+              else
+                    this.WindowState = WindowState.Maximized;
+
+            };
+            btnClose.Click += (s, e) => { this.Close(); };
+
+
+            ColorZone.MouseMove += (s, e) =>//鼠标左键按下时，程序界面跟随鼠标指针
+            {
+                if(e.LeftButton== MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+
+            ColorZone.MouseDoubleClick += (s, e) =>//鼠标双击依据情况放大还是回归正常
+            {
+                if (this.WindowState == WindowState.Normal)
+                    this.WindowState = WindowState.Maximized;
+                else
+                    this.WindowState = WindowState.Normal;
+            };
+
+
+
         }
     }
 }
