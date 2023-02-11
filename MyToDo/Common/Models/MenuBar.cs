8c8382650e7ec50eff324c40dc42ed0a90d9cb14
BsKPLu <bianshuiguangzhong@gmail.com>
2023-02-11 17:45:53 +0800

    主界面菜单导航栏实现

--- MyToDo/Common/Models/MenuBar.cs
+++ MyToDo/Common/Models/MenuBar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace MyToDo.Common.Models
 {
-    public class MenuBar
+	//系统导航菜单实体类
+    public class MenuBar:BindableBase
     {
 		private String? icon;
 
-		public String Icon
+		public String? Icon
 		{
 			get { return icon; }
 			set { icon = value; }
@@ -18,11 +20,21 @@ namespace MyToDo.Common.Models
 
 		private String? title;
 
-		public String Title
+		public String? Title
 		{
 			get { return title; }
 			set { title = value; }
+
 		}
+
+		private String? nameSpace;
+
+		public String? NameSpace
+		{
+			get { return nameSpace; }
+			set { nameSpace = value; }
+		}
+
 
 
 
--- MyToDo/Views/MainView.xaml.cs
+++ MyToDo/Views/MainView.xaml.cs
@@ -10,43 +10,37 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyToDo
+namespace MyToDo.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
-
-            /*
-             * 界面按钮操作实现逻辑
-             */
             btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
-            btnMax.Click += (s, e) => 
-            { if(this.WindowState == WindowState.Maximized)
-                    this.WindowState = WindowState.Normal;
-              else
-                    this.WindowState = WindowState.Maximized;
-
-            };
-            btnClose.Click += (s, e) => { this.Close(); };
-
-
-            ColorZone.MouseMove += (s, e) =>//鼠标左键按下时，程序界面跟随鼠标指针
+            btnMax.Click += (s, e) =>
             {
-                if(e.LeftButton== MouseButtonState.Pressed)
-                {
+                if (this.WindowState == WindowState.Maximized)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += (s, e) =>
+            {
+                this.Close();
+            };
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
                     this.DragMove();
-                }
             };
 
-            ColorZone.MouseDoubleClick += (s, e) =>//鼠标双击依据情况放大还是回归正常
+            ColorZone.MouseDoubleClick += (s, e) =>
             {
                 if (this.WindowState == WindowState.Normal)
                     this.WindowState = WindowState.Maximized;
@@ -54,8 +48,10 @@ namespace MyToDo
                     this.WindowState = WindowState.Normal;
             };
 
-
-
+            menuBar.SelectionChanged += (s, e) =>
+            {
+                drawerHost.IsLeftDrawerOpen = false;
+            };
         }
     }
 }
