b92f749008d1c50fdae2fb99e7fb5e819a072ded
BsKPLu <bianshuiguangzhong@gmail.com>
2023-02-11 13:40:31 +0800

    菜单model类创建

---
+++ MyToDo/Common/Models/MenuBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.Common.Models
+{
+    public class MenuBar
+    {
+		private String? icon;
+
+		public String Icon
+		{
+			get { return icon; }
+			set { icon = value; }
+		}
+
+		private String? title;
+
+		public String Title
+		{
+			get { return title; }
+			set { title = value; }
+		}
+
+
+
+
+	}
+}
