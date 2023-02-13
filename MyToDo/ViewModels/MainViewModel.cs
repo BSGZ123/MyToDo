48c4d9d3f0c1f08b33b4c1bff0e845e69dad068c
BsKPLu <bianshuiguangzhong@gmail.com>
2023-02-13 18:52:00 +0800

    页面跳转

--- MyToDo/ViewModels/MainViewModel.cs
+++ MyToDo/ViewModels/MainViewModel.cs
@@ -1,5 +1,10 @@
-﻿using MyToDo.Common.Models;
+﻿using MyToDo.Common;
+using MyToDo.Common.Models;
+using MyToDo.Extensions;
+using Prism.Commands;
+using Prism.Ioc;
 using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,12 +16,43 @@ namespace MyToDo.ViewModels
 {
     public class MainViewModel:BindableBase
     {
-        public MainViewModel()
+        public MainViewModel(IRegionManager regionManager)
         {
             MenuBars = new ObservableCollection<MenuBar>();
             CreateMenuBar();
+            NavigateCommand = new DelegateCommand<MenuBar>(Navigate);
+            this.regionManager = regionManager;
+
+            GoBackCommand = new DelegateCommand(() =>
+            {
+                if (journal != null && journal.CanGoBack)
+                    journal.GoBack();
+            });
+            GoForwardCommand = new DelegateCommand(() =>
+            {
+                if (journal != null && journal.CanGoForward)
+                    journal.GoForward();
+            });
         }
 
+        private void Navigate(MenuBar obj)
+        {
+            if (obj == null || string.IsNullOrWhiteSpace(obj.NameSpace))
+                return;
+            regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(obj.NameSpace, back =>
+            {
+                journal = back.Context.NavigationService.Journal;
+            });
+
+
+        }
+
+        public DelegateCommand<MenuBar> NavigateCommand { get; private set; }
+        public DelegateCommand GoBackCommand { get; private set; }
+        public DelegateCommand GoForwardCommand { get; private set; }
+
+        private IRegionNavigationJournal journal;
+        private readonly IRegionManager regionManager;
         private ObservableCollection<MenuBar> menuBars;
 
         public ObservableCollection<MenuBar> MenuBars
@@ -28,7 +64,7 @@ namespace MyToDo.ViewModels
         void CreateMenuBar()
         {
             MenuBars.Add(new MenuBar() { Icon = "Home", Title = "首页", NameSpace = "IndexView" });
-            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSpace = "ToDOView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookOutline", Title = "待办事项", NameSpace = "ToDoView" });
             MenuBars.Add(new MenuBar() { Icon = "NotebookPlus", Title = "备忘录", NameSpace = "MemoView" });
             MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSpace = "SettingsView" });
         }
